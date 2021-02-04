01c684e2052461866a7301687457312935d3e9d2
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-02-05 03:49:20 +0530

    add footer

--- src/components/footer/footer-component.jsx
+++ src/components/footer/footer-component.jsx
@@ -3,7 +3,7 @@ import './footer-style.scss';
 const Footer = () => {
     return (
         <div className="footer">
-            <h1 className='text-right text-muted size14'>Designed & Developed by Shreyash Kumar Singh</h1>
+            <h1 className='text-right my-0 text-muted size14'>Designed & Developed by Shreyash Kumar Singh</h1>
         </div>
     )
 }
