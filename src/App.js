9aa48bdacfb827b88d35bceb6fc9c3cf20cd6b25
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-02-09 16:13:32 +0530

    add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Header from './components/header/header-component';
 import Homepage from './pages/homepage/homepage';
+import Footer from './components/footer/footer-component';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
         <Switch>
           <Route path="/" exact component={ Homepage }/>
         </Switch>
+        <Footer />
       </div>
     )
   }
